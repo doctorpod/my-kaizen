6883801142d62eeffd853a2e286a03b9abe28a43
Andy White <andy@wireworldmedia.co.uk>
2019-05-02 21:39:36 +0100

    Fix recent average bug

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,18 +5,30 @@ class Card < ApplicationRecord
   AVG_RANGE = 10
 
   def score_today(date)
-    format period_summaries.where(date: date).sum(:score)
+    format recent_scores(date)[date] || 0.0
   end
 
   def score_yesterday(date)
-    format period_summaries.where(date: date-1).sum(:score)
+    format recent_scores(date)[date-1] || 0.0
   end
 
   def recent_average(date)
-    format period_summaries.where(date: (date-(AVG_RANGE+1)..date-1)).sum(:score) / AVG_RANGE.to_f
+    recent = recent_scores(date).select { |dt, _score| dt < (date - 1) }
+    return "0.00" if recent.empty?
+
+    total = recent.inject(0) { |memo, (_date, score)| memo += score }
+    format total.to_f / recent.size
   end
 
   private
+
+  def recent_scores(date)
+    @recent_scores ||= period_summaries
+      .where('date >= ?', date-(AVG_RANGE+1))
+      .group(:date)
+      .order(:date)
+      .sum(:score)
+  end
 
   def format(num)
     "%.2f" % num
