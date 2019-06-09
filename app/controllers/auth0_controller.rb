4cdb08573bbcde140d9677bb2b3a0ac699969daa
Andy White <andy@wireworldmedia.co.uk>
2019-06-09 22:18:33 +0100

    Auth0 controller & routes

---
+++ app/controllers/auth0_controller.rb
@@ -0,0 +1,43 @@
+class Auth0Controller < ApplicationController
+  def callback
+    # This stores all the user information that came from Auth0
+    # and the IdP
+    session[:userinfo] = request.env['omniauth.auth']
+
+    # Redirect to the URL you want after successful auth
+    redirect_to cards_url
+  end
+
+  def failure
+    # show a failure page or redirect to an error page
+    @error_msg = request.params['message']
+  end
+
+  def logout
+    reset_session
+    redirect_to logout_url.to_s
+  end
+
+  private
+
+  def logout_url
+    request_params = {
+      returnTo: root_url,
+      client_id: client_id
+    }
+
+    URI::HTTPS.build(host: domain, path: '/v2/logout', query: to_query(request_params))
+  end
+
+  def to_query(hash)
+    hash.map { |k, v| "#{k}=#{URI.escape(v)}" unless v.nil? }.reject(&:nil?).join('&')
+  end
+
+  def domain
+    ENV['OAUTH_DOMAIN']
+  end
+
+  def client_id
+    ENV['OAUTH_CLIENT_ID']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+  get '/', to: 'home#index'
+
   get '/cards/deck', to: 'cards#deck'
   resources :cards
+
   resources :items
+
   post '/checks', to: 'check#create'
-  get '/dashboard/index'
+
   # Needed to overcome the issue of dropdowns intermitently not working on reloading of cards page
   put '/cancel', to: redirect('/cards', status: 302)
 
-  # Needed to overcome the issue of dropdowns intermitently not working on reloading of dashboard page
-  put '/cancel', to: redirect('/', status: 302)
+  # Oauth routes
+  get '/auth/oauth2/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/logout' => 'auth0#logout'
 
   root 'home#index'
 end
